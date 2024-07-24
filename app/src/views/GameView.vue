142c67122dcd3848b3b6ed51d2b6d55e57197582
Justin Radatti <jdradatti@gmail.com>
2024-07-24 10:13:26 -0700

    Update GameView slightly

--- app/src/views/GameView.vue
+++ app/src/views/GameView.vue
@@ -3,20 +3,25 @@ import GameBoard from '../components/GameBoard.vue'
 
 if (window["WebSocket"]) {
     var conn = new WebSocket("ws://" + document.location.host + "/game/123");
-    console.log(conn)
-    conn.onclose = function (evt) {
+    conn.onclose = function (event) {
         var item = document.createElement("div");
         item.innerHTML = "<b>Connection closed.</b>";
-        console.log(item);
+        console.log("CONNECTIKON CLOSED");
     };
-    conn.onmessage = function (evt) {
-        var messages = evt.data.split('\n');
+    conn.onmessage = function (event) {
+        var messages = event.data.split('\n');
         for (var i = 0; i < messages.length; i++) {
             var item = document.createElement("div");
             item.innerText = messages[i];
-            console.log(item);
+            console.log("MESSAGE", messages[i]);
         }
     };
+
+    conn.onopen = function (event) {
+        conn.send("Here's some text that the server is urgently awaiting!");
+    }
+
+
 } else {
     var item = document.createElement("div");
     item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
