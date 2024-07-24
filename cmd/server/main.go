9f9279dcce5cb7ded46830b0f38628cca859775d
Justin Radatti <jdradatti@gmail.com>
2024-07-24 09:17:57 -0700

    Upgrade server to handle websocket connections

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,14 +4,22 @@ import (
 	"flag"
 	"log"
 	"net/http"
+    "github.com/JDRadatti/reptile/internal/websocket"
 )
 
-var addr = flag.String("-addr", ":3000", "http server address")
+var addr = flag.String("addr", ":3000", "http server address")
 
 func serveHome() {
-	http.Handle("/", http.FileServer(http.Dir("../app/dist")))
-	log.Println("http server listening from", addr)
-	log.Panic(http.ListenAndServe(*addr, nil))
+    router := http.NewServeMux()
+	router.HandleFunc("GET /game/{id}", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("game id", r)
+        idString := r.PathValue("id")
+        log.Println(idString)
+        websocket.ServeWebSocket(w, r)
+	})
+	router.Handle("/", http.FileServer(http.Dir("app/dist")))
+	log.Println("http server listening from", *addr)
+	log.Panic(http.ListenAndServe(*addr, router))
 }
 
 func main() {
