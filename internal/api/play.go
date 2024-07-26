c7abc87225971f03e3d27e8113b2d1376059285f
Justin Radatti <jdradatti@gmail.com>
2024-07-26 10:39:38 -0700

    Update handler for play.go. will delete later

--- internal/api/play.go
+++ internal/api/play.go
@@ -1,27 +1,27 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/JDRadatti/reptile/internal/websocket"
-	"log"
 	"net/http"
-    "encoding/json"
 )
 
 type GameOptions struct {
-    Time int
-    Increment int
+	Time      int
+	Increment int
 }
 
 // HandlePlay handles online game requests
 func HandlePlay(w http.ResponseWriter, r *http.Request, lobby *websocket.Lobby) {
 
-    var options GameOptions
-    err := json.NewDecoder(r.Body).Decode(&options)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-	log.Println(options)
-    gameID := "123"
-    w.Write([]byte(gameID))
+	var options GameOptions
+	err := json.NewDecoder(r.Body).Decode(&options)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//player := websocket.NewPlayer(lobby)
+	//lobby.PlayerPool <- player
+    //go player.WaitForGame(w)
 }
