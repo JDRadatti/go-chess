6344d3754a4533bbc85bfd5c3582789f8db4f934
Justin Radatti <jdradatti@gmail.com>
2024-07-26 10:44:18 -0700

    Update websocket.go

--- internal/websocket/websocket.go
+++ internal/websocket/websocket.go
@@ -11,15 +11,21 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func ServeWebSocket(w http.ResponseWriter, r *http.Request, l *Lobby) {
+func ServeWebSocket(w http.ResponseWriter, r *http.Request, l *Lobby, gameID string) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	player := newPlayer(l, conn)
-    l.PlayerPool <- player
+    player := NewPlayer(l, conn)
+    l.PlayerPool <-player
+    if player.Game != nil {
+        // Player already in game, check gameIDs match
+        // check client id's match
+        // send redirect to the current url
+        log.Printf("player %s already in game %s but requested %s", player.ID, player.Game.ID, gameID)
+    }
 
 	go player.write()
 	go player.read()
