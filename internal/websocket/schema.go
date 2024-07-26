6afab5aebb43032862304d3754a88e0989a39663
Justin Radatti <jdradatti@gmail.com>
2024-07-26 11:47:13 -0700

    Add gameID to all schema structs in schema.go

--- internal/websocket/schema.go
+++ internal/websocket/schema.go
@@ -3,18 +3,21 @@ package websocket
 // MoveRequest is sent from client to request a move
 type MoveRequest struct {
 	PlayerID string
+	GameID   string
 	Move     string
 }
 
 // MoveAccept is sent from the server when a move is accepted
 type MoveAccept struct {
 	PlayerID string
+	GameID   string
 	Move     string
 }
 
 // MoveAccept is sent from the server when a move is denied
 type MoveError struct {
 	PlayerID string
+	GameID   string
 	Move     string
 	Err      error
 }
@@ -35,5 +38,6 @@ type GameAccepted struct {
 // GameError is sent from the client when joining a game fails
 type GameError struct {
 	PlayerID string
+	GameID   string
 	Err      string
 }
