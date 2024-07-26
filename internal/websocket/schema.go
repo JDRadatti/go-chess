ed25843ca80ce861a754473aaca197fc7782ec2e
Justin Radatti <jdradatti@gmail.com>
2024-07-26 10:44:44 -0700

    Add schema.go to standardize messages going in and out

---
+++ internal/websocket/schema.go
@@ -0,0 +1,45 @@
+package websocket
+
+import (
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+// MoveRequest is sent from client to request a move
+type MoveRequest struct {
+	PlayerID string
+	Move     string
+}
+
+// MoveAccept is sent from the server when a move is accepted
+type MoveAccept struct {
+	PlayerID string
+	Move     string
+}
+
+// MoveAccept is sent from the server when a move is denied
+type MoveError struct {
+	PlayerID string
+	Move     string
+	Err      error
+}
+
+// GameRequest is sent from the client when wanting to join a game
+type GameRequest struct {
+	PlayerID  string
+	Time      int
+	Increment int
+}
+
+// GameAccepted is sent from the client when wanting to join a game
+type GameAccepted struct {
+	PlayerID string
+	GameID   string
+}
+
+// GameError is sent from the client when joining a game fails
+type GameError struct {
+	PlayerID string
+	Err      string
+}
