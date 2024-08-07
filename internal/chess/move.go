f08dbbe527353940a99e4952f744c87d266e3e13
Justin Radatti <jdradatti@gmail.com>
2024-08-07 11:54:09 -0700

    Fix bug in move.go

--- internal/chess/move.go
+++ internal/chess/move.go
@@ -54,9 +54,9 @@ func move(start *Square, dest *Square) (int, int) {
 	} else if startFile == destFile && start.index < dest.index {
 		return SOUTH, destRank - startRank
 	} else if startRank == destRank && start.index > dest.index {
-		return EAST, destRank - startRank
+		return WEST, destFile - startFile
 	} else if startRank == destRank && start.index < dest.index {
-		return WEST, startRank - destRank
+		return EAST, startFile - destFile
 	}
 
 	rankDiff := startRank - destRank
