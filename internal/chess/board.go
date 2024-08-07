2d6e80b0276bb2cf9dc6a6177b804cc4c345f6e3
Justin Radatti <jdradatti@gmail.com>
2024-08-07 08:39:44 -0700

    Add comment for Board to explain Board implementation

--- internal/chess/board.go
+++ internal/chess/board.go
@@ -13,6 +13,18 @@ const (
 	NUM_SQUARES int = 64
 )
 
+// Board represents the state of a chess game.
+// Board uses a 1D array with indicies as shown below.
+//
+//	8   0   1   2   3   4   5   6   7
+//	7   8   9  10  11  12  13  14  15
+//	6  16  17  18  19  20  21  22  23
+//	5  24  25  26  27  28  29  30  31
+//	4  32  33  34  35  36  37  38  39
+//	3  40  41  42  43  44  45  46  47
+//	2  48  49  50  51  52  53  54  55
+//	1  56  57  58  59  60  61  62  63
+//	   a   b   c   d   e   f   g   h
 type Board struct {
 	squares   [NUM_SQUARES]Square
 	turn      Player // 0 for white, 1 for black
