680ca60e4d1a96d4c5af46de9333f5c6998d8335
Justin Radatti <jdradatti@gmail.com>
2024-08-20 13:12:34 -0700

    Update frontend to stop using /token endpoint

--- app/src/scripts/api.js
+++ app/src/scripts/api.js
@@ -1,29 +1,15 @@
 import axios from 'axios'
-import { ref } from 'vue'
 
 const defaultTime = 10
 const defaultIncrement = 0
-const playerID = ref("")
 
 // Get the PlayerID, or retrieve a new PlayerID from the server
 export function getPlayerID() {
-    if (playerID.value != "") {
-        return playerID.value
-    }
-    if (localStorage.getItem("playerID") != null) {
-        playerID.value = localStorage.getItem("playerID")
-        return playerID.value
-    }
-
-    return playerID.value
+    return localStorage.getItem("playerID")
 }
 
-export function setPlayerID() {
-    axios.post("/token").then(response => {
-        localStorage.setItem("playerID", response.data)
-    }).catch(err => {
-        console.log("error", err)
-    })
+export function setPlayerID(playerID) {
+    localStorage.setItem("playerID", playerID)
 }
 
 
@@ -34,6 +20,8 @@ export async function startGame(time, increment) {
         time: ((time) ? time : defaultTime),
         increment: ((increment) ? increment : defaultIncrement),
     }).then(response => {
+        console.log("playerID", response.data.PlayerID)
+        setPlayerID(response.data.PlayerID)
         return response.data
     }).catch(error => {
         console.log("ERROR: ", error)
