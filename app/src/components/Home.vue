ca25bc59dd51d5040b30c534f3253efb2385eef4
Justin Radatti <jdradatti@gmail.com>
2024-12-04 10:44:46 -0800

    Add portfolio

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -2,10 +2,44 @@
 </script>
 
 <template>
-  <div>
-    <h1 class="color-primary">Welcome</h1>
-  </div>
+    <div>
+        <h1 class="color-primary">Justin Radatti</h1>
+        <hr/>
+        <h3>Experience</h3>
+        <ul>
+            <li><div class="experience-container">Differentiated Therapeutics <small>Software Engineer (2+ Years)</small></div></li>
+            <li>Deschutes Public Library Foundation Website [In Progress]: <a href="https://main.d3gasdxvwq15bg.amplifyapp.com/">https://main.d3gasdxvwq15bg.amplifyapp.com/</a></li>
+            <li>Deschutes Endodontics Website: <a href="https://deschutesendo.com/">https://deschutesendo.com/</a></li>
+            <li><div class="experience-container"> Draco Ova Holdings <small>Software Engineer Intern (3 Months)</small></div></li>
+            <li><div class="experience-container">RockBot <small>Software Engineer Intern (3 Months)</small></div></li>
+        </ul>
+        <hr/>
+        <h3>Projects</h3>
+        <ul>
+            <li>Multiplayer chess game. Click "Play" on left.</li>
+            <li>Computer Graphics: <a href="https://cal-cs184-student.github.io/sp22-project-webpages-JDRadatti/">https://cal-cs184-student.github.io/sp22-project-webpages-JDRadatti/</a></li>
+            <li>RAD CSS: <a href="https://main.d3qz4ocyf7g8uf.amplifyapp.com/">https://main.d3qz4ocyf7g8uf.amplifyapp.com/</a></li>
+        </ul>
+        <hr/>
+        <h3>Education</h3>
+        <h4>University of California, Berkeley</h4>
+        <ul>
+            <li>Operating Systems & System Programming (C)</li> 
+            <li>Computer Graphics (C++)</li> 
+            <li>Efficient Algorithms and Intractable problems</li>
+            <li>Database Systems (C++, SQL)</li> 
+            <li>Artificial Intelligence (Python)</li> 
+            <li>Computer Security (Go)</li> 
+            <li>Discrete Math and Probability Theory</li> 
+            <li>Data Structures (Java)</li>
+        </ul>
+        <hr/>
+        <h3>Links</h3>
+        <ul>
+            <li><a href="https://Github.com/JDRadatti">GitHub</a></li>
+            <li><a href="www.linkedin.com/in/justin-radatti-000">LinkedIn</a></li>
+        </ul>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
